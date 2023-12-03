f8ba7169ff899835b04529650de0962b37caa753
Marcelo Buongiorno <mnbuongiorno@gmail.com>
2023-12-02 22:15:04 -0300

    Primer commit

---
+++ src/routes/books.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const libroController = require('../controllers/libroController');
+
+// Rutas para los libros
+router.get('/', libroController.obtenerLibros);
+router.get('/:id', libroController.obtenerLibroPorId);
+router.post('/', libroController.crearLibro);
+router.put('/:id', libroController.actualizarLibro);
+router.delete('/:id', libroController.eliminarLibro);
+
+module.exports = router;
