7be4abdd720c622b434ee6f18f30de18a51dd827
Antonelladrc <antonelladiroco7@gmail.com>
2023-12-04 00:57:22 -0300

    push

--- app.js
+++ app.js
@@ -56,7 +56,14 @@ app.use("/api", cors(), apiRouter);
 /* catch 404 */
 app.use(function(req, res, next){
     next(createError(404));
-})
+});
 
+app.use(funtion(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err: {};
+    
+    res.status(err.status || 500);
+    res.render("error");
+});
 
 module.exports = app;
