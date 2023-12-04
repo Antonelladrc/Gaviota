8501011deac64622a55c8dec99f6a61771f6f1b2
Antonelladrc <antonelladiroco7@gmail.com>
2023-12-04 00:57:44 -0300

    correction

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(req, res, next){
     next(createError(404));
 });
 
-app.use(funtion(err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err: {};
     
