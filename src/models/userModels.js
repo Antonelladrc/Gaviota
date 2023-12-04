4d2b45905e27ce653a25bf3e41f030d673cb9058
Antonelladrc <antonelladiroco7@gmail.com>
2023-12-04 01:42:38 -0300

    tercero

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -1,3 +1,23 @@
+/* este código proporciona una función para autenticar a los usuarios mediante la búsqueda de un 
+nombre de usuario y una contraseña coincidentes en una base de datos */
+
+var pool = require('./bd');
+var md5 = require('md5');
+
+async function getUserByUsernameAndPassword(user, password){
+    try{
+        var query = 'select * from usuario where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query,[user, md5(password)]);
+        return rows[0];
+    } catch (error){
+        console.log(error);
+    }
+ }
+
+ module.exports = {getUserByUsernameAndPassword}
+
+/* 
+var pool = require("")
 const {DataTypes} = require ('sequelize')
 const sequelize = require ('./db')
 
@@ -26,4 +46,4 @@ const User= sequelize.define("User", {
     }
 });
 
-User.sync();
+User.sync(); */
